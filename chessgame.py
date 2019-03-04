4f649929c0166b8c37b68c26ed552d4ae8c12660
Fin <spamsuckersunited@gmail.com>
2019-03-03 20:54:47 -0700

    Local dir

--- chessgame.py
+++ chessgame.py
@@ -19,7 +19,7 @@ class InvalidMoveException(Exception):
 
 class ChessGame:
     def __init__(self):
-        self.engine = chess.uci.popen_engine("stockfish_10_x64_popcnt") if os.name == 'nt' else chess.uci.popen_engine('stockfish_10_x64_modern')
+        self.engine = chess.uci.popen_engine("stockfish_10_x64_popcnt") if os.name == 'nt' else chess.uci.popen_engine('./stockfish_10_x64_modern')
         self.engine.uci()
         self.board = chess.Board()
         self.engine.position(self.board)
