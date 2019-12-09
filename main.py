09d5c9cfedf45a095266d36dabe0a6504a491817
Fin <spamsuckersunited@gmail.com>
2019-12-08 17:19:21 -0700

    Help command remove docstrings

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ import aiohttp
 import datetime
 import json
 import io
-import itertools
 import regex
 import inspect
 import chessgame
@@ -26,9 +25,11 @@ import randomart
 import copy
 import astar
 import zlib
+import itertools
 from fuzzywuzzy import fuzz
 from discord import *
 from discord.ext.commands import *
+from discord import utils
 
 default_config = '''
 prefix = ('f?', 'f!', 'F?', 'F!')
@@ -80,83 +81,181 @@ except ImportError:
         f.write(default_config)
     sys.exit()
 
-"""
-class ProperHelp(HelpFormatter):
-    async def format(self):
-        Handles the actual behaviour involved with formatting.
-        To change the behaviour, this method should be overridden.
 
-        :return: List: A paginated value of the help command
-        self._paginator = Paginator()
+class ReplacementHelpCommand(HelpCommand):
 
-        # we need a padding of ~80 or so
+    def __init__(self, **options):
+        self.width = options.pop('width', 80)
+        self.indent = options.pop('indent', 2)
+        self.sort_commands = options.pop('sort_commands', True)
+        self.dm_help = options.pop('dm_help', False)
+        self.dm_help_threshold = options.pop('dm_help_threshold', 1000)
+        self.commands_heading = options.pop('commands_heading', "Commands:")
+        self.no_category = options.pop('no_category', 'No Category')
+        self.paginator = options.pop('paginator', None)
 
-        description = self.command.description if not self.is_cog() else inspect.getdoc(self.command)
+        if self.paginator is None:
+            self.paginator = Paginator()
 
-        if description:
-            # <description> portion
-            self._paginator.add_line(description, empty=True)
+        super().__init__(**options)
 
-        if isinstance(self.command, Command):
-            # <signature portion>
-            signature = self.get_command_signature()
-            self._paginator.add_line(signature, empty=True)
+    def shorten_text(self, text):
+        """Shortens text to fit into the :attr:`width`."""
+        if len(text) > self.width:
+            return text[:self.width - 3] + '...'
+        return text
 
-            # Don't want to include the docstring
-            '''# <long doc> section
-            if self.command.help:
-                self._paginator.add_line(self.command.help, empty=True)
-            '''
+    def get_ending_note(self):
+        """Returns help command's ending note. This is mainly useful to override for i18n purposes."""
+        command_name = self.invoked_with
+        return "Type {0}{1} command for more info on a command.\n" \
+               "You can also type {0}{1} category for more info on a category.".format(self.clean_prefix, command_name)
 
-            # end it here if it's just a regular command
-            if not self.has_subcommands():
-                self._paginator.close_page()
-                return self._paginator.pages
+    def add_indented_commands(self, commands, *, heading, max_size=None):
+        """Indents a list of commands after the specified heading.
+        The formatting is added to the :attr:`paginator`.
+        The default implementation is the command name indented by
+        :attr:`indent` spaces, padded to ``max_size`` followed by
+        the command's :attr:`Command.short_doc` and then shortened
+        to fit into the :attr:`width`.
+        Parameters
+        -----------
+        commands: Sequence[:class:`Command`]
+            A list of commands to indent for output.
+        heading: :class:`str`
+            The heading to add to the output. This is only added
+            if the list of commands is greater than 0.
+        max_size: Optional[:class:`int`]
+            The max size to use for the gap between indents.
+            If unspecified, calls :meth:`get_max_size` on the
+            commands parameter.
+        """
 
-        max_width = self.max_name_size
+        if not commands:
+            return
 
-        def category(tup):
-            cog = tup[1].cog_name
-            # we insert the zero width space there to give it approximate
-            # last place sorting position.
-            return cog + ':' if cog is not None else '\u200bNo Category:'
+        self.paginator.add_line(heading)
+        max_size = max_size or self.get_max_size(commands)
 
-        filtered = await self.filter_command_list()
-        if self.is_bot():
-            data = sorted(filtered, key=category)
-            for category, commands in itertools.groupby(data, key=category):
-                # there simply is no prettier way of doing this.
-                commands = sorted(commands)
-                if len(commands) > 0:
-                    self._paginator.add_line(category)
+        get_width = utils._string_width
+        for command in commands:
+            name = command.name
+            width = max_size - (get_width(name) - len(name))
+            entry = '{0}{1:<{width}} {2}'.format(self.indent * ' ', name, command.short_doc, width=width)
+            self.paginator.add_line(self.shorten_text(entry))
 
-                self._add_subcommands_to_page(max_width, commands)
+    async def send_pages(self):
+        """A helper utility to send the page output from :attr:`paginator` to the destination."""
+        destination = self.get_destination()
+        for page in self.paginator.pages:
+            await destination.send(page)
+
+    def get_command_signature(self, command):
+        parent = command.full_parent_name
+        if len(command.aliases) > 0:
+            aliases = '|'.join(command.aliases)
+            fmt = '[%s|%s]' % (command.name, aliases)
+            if parent:
+                fmt = parent + ' ' + fmt
+            alias = fmt
         else:
-            filtered = sorted(filtered)
-            if filtered:
-                self._paginator.add_line('Commands:')
-                self._add_subcommands_to_page(max_width, filtered)
+            alias = command.name if not parent else parent + ' ' + command.name
 
-        # add the ending note
-        self._paginator.add_line()
-        ending_note = self.get_ending_note()
-        self._paginator.add_line(ending_note)
-        return self._paginator.pages
-"""
+        return '%s%s %s' % (self.clean_prefix, alias, command.signature)
 
+    def add_command_formatting(self, command):
+        """A utility function to format the non-indented block of commands and groups.
+        Parameters
+        ------------
+        command: :class:`Command`
+            The command to format.
+        """
 
-class ReplaceMentHelpCommand(DefaultHelpCommand):
-    pass
+        signature = self.get_command_signature(command)
+        self.paginator.add_line(signature, empty=True)
 
+    def get_destination(self):
+        ctx = self.context
+        if self.dm_help is True:
+            return ctx.author
+        elif self.dm_help is None and len(self.paginator) > self.dm_help_threshold:
+            return ctx.author
+        else:
+            return ctx.channel
 
+    async def prepare_help_command(self, ctx, command):
+        self.paginator.clear()
+        await super().prepare_help_command(ctx, command)
 
-#formatter = ProperHelp()
+    async def send_bot_help(self, mapping):
+        ctx = self.context
+        bot = ctx.bot
+
+        if bot.description:
+            # <description> portion
+            self.paginator.add_line(bot.description, empty=True)
+
+        no_category = '\u200b{0.no_category}:'.format(self)
+
+        def get_category(command, *, no_category=no_category):
+            cog = command.cog
+            return cog.qualified_name + ':' if cog is not None else no_category
+
+        filtered = await self.filter_commands(bot.commands, sort=True, key=get_category)
+        max_size = self.get_max_size(filtered)
+        to_iterate = itertools.groupby(filtered, key=get_category)
+
+        # Now we can add the commands to the page.
+        for category, commands in to_iterate:
+            commands = sorted(commands, key=lambda c: c.name) if self.sort_commands else list(commands)
+            self.add_indented_commands(commands, heading=category, max_size=max_size)
+
+        note = self.get_ending_note()
+        if note:
+            self.paginator.add_line()
+            self.paginator.add_line(note)
+
+        await self.send_pages()
+
+    async def send_command_help(self, command):
+        self.add_command_formatting(command)
+        self.paginator.close_page()
+        await self.send_pages()
+
+    async def send_group_help(self, group):
+        self.add_command_formatting(group)
+
+        filtered = await self.filter_commands(group.commands, sort=self.sort_commands)
+        self.add_indented_commands(filtered, heading=self.commands_heading)
+
+        if filtered:
+            note = self.get_ending_note()
+            if note:
+                self.paginator.add_line()
+                self.paginator.add_line(note)
+
+        await self.send_pages()
+
+    async def send_cog_help(self, cog):
+        if cog.description:
+            self.paginator.add_line(cog.description, empty=True)
+
+        filtered = await self.filter_commands(cog.get_commands(), sort=self.sort_commands)
+        self.add_indented_commands(filtered, heading=self.commands_heading)
+
+        note = self.get_ending_note()
+        if note:
+            self.paginator.add_line()
+            self.paginator.add_line(note)
+
+        await self.send_pages()
+
 
 client = Bot(command_prefix=config.prefix,
              description='''A bot written by Finianb1 for use in various discord servers.
               Can play chess, roll dice, and track initiative, among other things.''')
 
-print(client.help_command)
+client.help_command = ReplacementHelpCommand()
 
 
 def format_large(number):
@@ -390,10 +489,10 @@ async def top(context):
         for i in range(10):
             if len(userList[i].display_name) > 20:
                 ranking += '%s. %s...: %s levels, %s xp\n' % (
-                i + 1, userList[i].display_name[:17], get_level(userList[i]), get_xp(userList[i]))
+                    i + 1, userList[i].display_name[:17], get_level(userList[i]), get_xp(userList[i]))
             else:
                 ranking += '%s. %s: %s levels, %s xp\n' % (
-                i + 1, userList[i].display_name, get_level(userList[i]), get_xp(userList[i]))
+                    i + 1, userList[i].display_name, get_level(userList[i]), get_xp(userList[i]))
         await context.send('Rankings for this server:\n```%s```' % ranking)
 
 
@@ -519,7 +618,7 @@ async def new(context):
 @new.command(description='Starts a game of chess with the bot. To end a game of chess, type \'end\' instead of '
                          'entering your move. You must enter your move within 5 minutes or the game will time out. ',
              brief='Start a game of chess as white. ')
-async def white(context, easymode: bool=False):
+async def white(context, easymode: bool = False):
     """
     Command to start a new game of chess vs AI as white
 
@@ -547,7 +646,8 @@ async def white(context, easymode: bool=False):
         while True:
             await context.send('Please enter your move in UCI format (eg. e2e4)')
             try:
-                movestr = await client.wait_for('message', check=lambda m: (m.author == context.author and m.channel == context.channel), timeout=300)
+                movestr = await client.wait_for('message', check=lambda m: (
+                        m.author == context.author and m.channel == context.channel), timeout=300)
             except asyncio.TimeoutError:
                 end = True
                 timed_out = True
@@ -610,7 +710,7 @@ async def white(context, easymode: bool=False):
 @new.command(description='Starts a game of chess with the bot. To end a game of chess, type \'end\' instead of '
                          'entering your move. You must enter your move within 5 minutes or the game will time out. ',
              brief='Start a game of chess as white.')
-async def black(context, easymode: bool=False):
+async def black(context, easymode: bool = False):
     """
     Command to start a new game of chess vs AI as black
 
@@ -646,7 +746,8 @@ async def black(context, easymode: bool=False):
         while True:
             await context.send('Please enter your move in UCI format (eg. e2e4)')
             try:
-                movestr = await client.wait_for('message', check=lambda m: (m.author == context.author and m.channel == context.channel), timeout=300)
+                movestr = await client.wait_for('message', check=lambda m: (
+                        m.author == context.author and m.channel == context.channel), timeout=300)
             except asyncio.TimeoutError:
                 end = True
                 timed_out = True
@@ -730,7 +831,9 @@ async def challenge(context, white: Member):
         while True:
             await context.send('%s, please enter your move in UCI format (eg. e2e4)' % white.mention)
             try:
-                move_str = await client.wait_for('message', check=lambda m: (m.author == white and m.channel == context.channel), timeout=300)
+                move_str = await client.wait_for('message',
+                                                 check=lambda m: (m.author == white and m.channel == context.channel),
+                                                 timeout=300)
             except asyncio.TimeoutError:
                 end = True
                 timed_out = True
@@ -760,7 +863,9 @@ async def challenge(context, white: Member):
         while True:
             await context.send('%s, please enter your move in UCI format (eg. e2e4)' % black.mention)
             try:
-                move_str = await client.wait_for('message', check=lambda m: (m.author == black and m.channel == context.channel), timeout=300)
+                move_str = await client.wait_for('message',
+                                                 check=lambda m: (m.author == black and m.channel == context.channel),
+                                                 timeout=300)
             except asyncio.TimeoutError:
                 end = True
                 timed_out = True
@@ -812,6 +917,7 @@ async def challenge(context, white: Member):
     await context.send(embed=embed)
 
     chess_game.engine.close()
+
 
 @client.command(
     description="Start a new initiative tracker session. Initiative tracking uses three commands: 'next', 'add', and 'remove.'\n"
